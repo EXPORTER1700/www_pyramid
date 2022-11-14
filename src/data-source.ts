4091545363975b3999ec839ef2515918f27cd2e6
Illya <i.stetsenko1998@gmail.com>
2022-11-14 11:37:25 +0200

    Add readme

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,15 @@
 import { join } from 'path';
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: join(__dirname, '../.env') });
 
 export default new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'www',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
   migrations: [join(__dirname, 'migrations/**/*{.ts,.js}')],
   synchronize: false,
